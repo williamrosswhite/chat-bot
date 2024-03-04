ec5f90e9fc3d25c6a2d42cb3f316d0d65d5404fa
W. Ross White <rwhite83@hotmail.com>
2024-03-04 10:25:46 -0800

    Add frontend

---
+++ frontend/src/components/TextField.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div v-for="(message, index) in history" :key="index">
+      <p :class="message.role">{{ message.content }}</p>
+    </div>
+  </div>
+  <div>
+    <textarea 
+      v-model="userPromptText" 
+      @keydown.enter="processPrompt" 
+      @keydown.enter.prevent 
+      placeholder="Enter something...">
+    </textarea>
+  </div>
+  <div>
+    <button @click="processPrompt">Process Prompt</button>
+  </div>
+</template>
+  
+  <script>
+  import axios from 'axios';
+  import { toRaw } from 'vue';
+  
+  export default {
+    data() {
+      return {
+        promptResponse: '',
+        history: []
+      }
+    },
+    methods: {
+      processPrompt() {
+        this.history.push({ role: 'user', content: this.userPromptText });
+        let historyArray = toRaw(this.history);
+        axios.post('http://localhost:5111/api/OpenAi', { 
+          messages: historyArray,
+        })
+        .then(response => {
+          console.log('tracking error: ', response)
+          if(response.data.error?.code === "rate_limit_exceeded") {
+            alert('Whoops!  Too eager!\nRequests per minute limit exceeded, give it a second to cool down.')
+          } else {
+            let extractedResponse = response.data.choices[0].message;
+            console.log('response: ', extractedResponse);
+            this.history.push({ role: extractedResponse.role, content: extractedResponse.content });
+            this.promptResponse = extractedResponse.content;
+            this.userPromptText = '';
+          }
+
+        })
+        .catch(error => console.error(error));
+      }
+    }
+  }
+  </script>
